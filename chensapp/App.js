97608a8afa8dc869aaf484481fedb86e17c7e4d4
RichendaChen <148382557+RichendaChen@users.noreply.github.com>
2024-03-01 17:49:34 +0000

    1.3.24

---
+++ chensapp/App.js
@@ -0,0 +1,14 @@
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Universal React with Expo</Text>
+    </View>
+  );
+}
